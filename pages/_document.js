bd34807452ca1cad861c09bb22f5308ef275971b
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2023-04-21 20:44:02 +0000

    adiciona o git .gitignore amend

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style>
+            {`
+              :root {
+                --red: red;
+              }
+            `}
+          </style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const HomeTitle = styled.h1`
+  color: var(--red);
+`;
+
+function home (){
+  return (
+    <div>
+      <HomeTitle>Voce esta sendo vigiado</HomeTitle>
+      <h2> vc tem um prazo de uma semnana pra conseguir isso. espero que não falhe! </h2>
+      <h3> aperte em "clique aqui"</h3>
+      <Link href="https://www.instagram.com/julimotoka/">clique aqui</Link>
+    </div>
+  );
+}
+
+export default home;
+
+
