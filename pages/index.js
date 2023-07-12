2d9ae16c7ac828bbd35a473e7022821697d9d6a0
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2023-07-12 02:28:54 +0000

    adiciona pages Thierry Avelino

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
-import Link from 'next/link';
-import styled from 'styled-components';
-
-const HomeTitle = styled.h1`
-  color: var(--red);
-`;
 
 function home (){
-  return (
-    <div>
-      <HomeTitle>Voce esta sendo vigiado</HomeTitle>
-      <h2> vc tem um prazo de uma semnana pra conseguir isso. espero que não falhe! </h2>
-      <h3> aperte em "cliqugit "</h3>
-      <Link href="https://www.instagram.com/julimotoka/">clique aqui</Link>
-    </div>
-  );
+  return <h1>Olá eu sou o thierry</h1>
 }
 
 export default home;
