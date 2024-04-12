6463ac3ec448b0bc4160b7ae6582e91441d75c3b
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2024-04-12 12:08:18 +0000

    envia dados

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,14 @@
-import { useState } from "react";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("http://localhost:3000/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome, email }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data.message); // Mensagem de sucesso do servidor
-        // Aqui você pode limpar o formulário ou exibir uma mensagem de sucesso
-        setNome(""); // Limpa o campo nome
-        setEmail(""); // Limpa o campo email
-      } else {
-        console.error("Falha ao enviar os dados");
-      }
-    } catch (error) {
-      console.error("Erro ao enviar os dados:", error);
-    }
-  };
-
+function home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Bem-vindo à minha página de apresentação!
-        </h1>
-
-        <p className={styles.description}>
-          Esta é uma página simples criada com React e Next.js
-        </p>
-
-        {/* Formulário para nome e e-mail */}
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div>
-            <label htmlFor="nome">Nome:</label>
-            <input
-              id="nome"
-              type="text"
-              value={nome}
-              onChange={(e) => setNome(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">E-mail:</label>
-            <input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <button type="submit">Enviar</button>
-        </form>
-      </main>
+    <div>
+      <HomeTitle>Theimy Avelino | te amo </HomeTitle>
+      <h2>seu filho querido Thierrys</h2>
+      <h3> aperte em "clique"</h3>
+      <Link href="https://www.instagram.com/p/CmNiafeLMsR5Q5qWf-2T22QJ761IVV6xusavps0/">
+        clique aqui
+      </Link>
     </div>
   );
-  
 }
+
+export default home;
