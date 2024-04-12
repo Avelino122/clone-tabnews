0b3f2d6a077bd1aebff27b0505d6dbf10d3da5b1
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2024-04-12 18:17:47 +0000

    fix test error/ add logic if in function somar

--- models/calc.js
+++ models/calc.js
@@ -1,6 +1,11 @@
 function somar(param1, param2) {
+  if (typeof param1 !== "number") {
+    return "Error param1";
+  }
+  if (typeof param2 !== "number") {
+    return "Error param2";
+  }
   return param1 + param2;
 }
-somar(5, 5);
 
 exports.somar = somar;
