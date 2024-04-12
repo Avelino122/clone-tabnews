31eacc368e47eb8436c46d4948fe4337a39752da
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2024-04-12 17:34:24 +0000

    fix code to revie = code

---
+++ testes/calc.test.js
@@ -0,0 +1,11 @@
+test("paramOne", () => {
+  console.log("create a test to valide a code");
+});
+
+test("paramTwo", () => {
+  console.log("heyy.... I'm thierrys");
+});
+
+test("one+one", () => {
+  expect(3).toBe(3);
+});
