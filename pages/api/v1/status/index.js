8b6f8a3c6ab9a329db978427c652f85cbd7bccb6
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2024-05-12 19:12:43 +0000

    add .env in project `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 import database from "../../../../infra/database";
 async function stats(request, response) {
-  const result = await database.query("CREATE TABLE t1 (col1 text, col2 int);");
+  const result = await database.query("SELECT 1 + 1  AS SUM");
   console.log(result);
   response.status(200).json("{Thierrys: 200}");
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,6 @@
-test("my", async () => {
+test("GET to /api/v1/contet should to return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  const status = console.log(response.status);
+  console.log(response.status);
   console.log(response.url);
-  expect(response.status).toBe(response.status);
+  expect(response.status).toBe(200);
 });
