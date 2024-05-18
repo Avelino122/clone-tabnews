ce87ff73d58a655652fb0c08bd83e783c93ed83b
Avelino122 <thierryavel1@gmail.com>
2024-05-18 22:10:07 +0100

    update  scripts to automate   `package.json`

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc818e3c105333f1"; }
+/******/ 		__webpack_require__.h = function() { return "3fb4356348ec849f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
