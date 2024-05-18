ee27bf07f1131584258a02c4f492db962d40b06e
Avelino122 <thierryavel1@gmail.com>
2024-05-19 00:31:23 +0100

    retorno api real values `get.teste.js` `/api/v1/index.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,18 @@
 import database from "/infra/database";
 async function stats(request, response) {
-  const result = await database.query("SELECT 1 + 1  AS SUM");
-  console.log(result);
-  response.status(200).json("{Thierrys: 200}");
+  const update_at =  new Date().toISOString()
+  const version =  await database.query("SELECT version();");
+  const versionDB = version.rows[0].version;
+  const connects = await database.query('SELECT * FROM pg_roles WHERE rolname = current_user;')
+  const active_connections = connects.rows[0].rolname
+  console.log(connects)
+  console.log(version)
+  console.log(active_connections)
+  response.status(200).json({
+    update_time: update_at,
+    version_db:  versionDB,
+    connects_db: active_connections
+  });
 }
 
 export default stats;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,13 @@ test("GET to /api/v1/contet should to return 200", async () => {
   console.log(response.status);
   console.log(response.url);
   expect(response.status).toBe(200);
+  
+  const responseBody = await response.json()
+  expect(responseBody.update_time).toBeDefined()
+  const parseupdate = new Date(responseBody.update_time).toISOString()
+  console.log(`parse:${parseupdate}`)
+  expect(responseBody.update_time).toEqual(parseupdate)
+  const versionResponse = responseBody.version_db
+  console.log(versionResponse)
+  expect(responseBody.version_db).toMatch(/16\.0/)
 });
