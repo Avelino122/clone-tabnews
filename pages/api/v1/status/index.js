4f8ba5f72397c04c709ae95e68d0c44a3fdd6fc0
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2025-01-18 23:14:27 +0000

    new credentials prd

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,36 +1,37 @@
 import database from "/infra/database";
 async function stats(request, response) {
-  const update_at =  new Date().toISOString()
-  const version =  await database.query("SELECT version();");
+  const update_at = new Date().toISOString();
+  const version = await database.query("SELECT version();");
   const versionDB = version.rows[0].version;
-  const connects = await database.query('SELECT * FROM pg_roles WHERE rolname = current_user;')
-  const active_connections = connects.rows[0].rolname
-  const max_connect = await database.query('SHOW max_connections;')
-  const pqmax_connect = max_connect.rows[0].max_connections
+  const connects = await database.query(
+    "SELECT * FROM pg_roles WHERE rolname = current_user;",
+  );
+  const active_connections = connects.rows[0].rolname;
+  const max_connect = await database.query("SHOW max_connections;");
+  const pqmax_connect = max_connect.rows[0].max_connections;
   const dbNameThierrys = process.env.POSTGRES_DB;
   const open_connections = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [dbNameThierrys]
+    values: [dbNameThierrys],
   });
 
-  const filterOpenConnect = open_connections.rows[0].count
+  const filterOpenConnect = open_connections.rows[0].count;
 
-  console.log(connects)
-  console.log(version)
-  console.log(active_connections)
-  console.log(pqmax_connect)
-  console.log(open_connections)
+  console.log(connects);
+  console.log(version);
+  console.log(active_connections);
+  console.log(pqmax_connect);
+  console.log(open_connections);
 
-  console.log(filterOpenConnect)
-  console.log(`db: ${dbNameThierrys}`)
+  console.log(filterOpenConnect);
+  console.log(`db: ${dbNameThierrys}`);
 
   response.status(200).json({
     update_time: update_at,
-    version_db:  versionDB,
+    version_db: versionDB,
     connects_db: active_connections,
     connect_max: parseInt(pqmax_connect),
-    open_connections: parseInt(filterOpenConnect)
-    
+    open_connections: parseInt(filterOpenConnect),
   });
 }
 
