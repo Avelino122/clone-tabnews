dc7a27f58a0c4b6d010edf2096778f4408415cfc
Thierry Avelino <112650215+Avelino122@users.noreply.github.com>
2025-01-18 12:20:03 +0000

    working in logs in node.js

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
-  let results; 
+  console.log("credentials postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  let results;
 
   try {
     await client.connect();
     results = await client.query(queryObject);
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
